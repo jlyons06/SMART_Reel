3b2c45995dd5245fd529d432ab169655e3898895
nzclapp <97115713+nzclapp@users.noreply.github.com>
2022-11-10 08:55:19 -0800

    Added IMU_R.cpp and IMU_R.h file to interface with tasks

---
+++ IMU_R.h
@@ -0,0 +1,21 @@
+/** @file IMU_R.h
+ *  This helps print an array
+ *  @author Nolan Clapp
+ *  @author JR Ridgely, added more documentation and functionality
+ *  @date 2022-Oct-26 Original file by Clapp
+ */
+
+#ifndef IMU_R_h
+#define IMU_R_h
+
+#include <Arduino.h> 
+#include "PrintStream.h"
+/** This class operates several print array funcitons with different inputs
+ */
+
+
+void IMU_setup();
+void IMU_get_data();
+
+
+#endif
