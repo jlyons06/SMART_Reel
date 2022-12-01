b4c177d164c59e15d0ebba9e0474c68b34583e14
jlyons06 <jlyons06@calpoly.edu>
2022-11-30 22:05:00 -0800

    Update shares.h

--- shares.h
+++ shares.h
@@ -10,7 +10,11 @@
 #ifndef _SHARES_H_
 #define _SHARES_H_
 
+<<<<<<< Updated upstream
 #include <Arduino.h>
+=======
+#include "Arduino.h"
+>>>>>>> Stashed changes
 #include "taskshare.h"
 
 // A share which holds the flag to begin the pattern of the minnow bait
