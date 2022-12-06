66cd611d6669d3a4bc4b7e469556745d1ab17c07
nzclapp <97115713+nzclapp@users.noreply.github.com>
2022-12-05 18:48:33 -0800

    Update Motor.h

--- Motor.h
+++ Motor.h
@@ -1,7 +1,6 @@
-/** @file IMU_R.h
- *  This helps print an array
+/** @file Motor.h
+ *  This is the .h file associated with our motor class
  *  @author Nolan Clapp
- *  @author JR Ridgely, added more documentation and functionality
  *  @date 2022-Oct-26 Original file by Clapp
  */
 
@@ -11,26 +10,32 @@
 #include <Arduino.h> 
 #include "PrintStream.h"
 
-/** @brief   Class which contains stat tracking data
+/** @brief   Class which deals with motor interaction
  */
 class Motor
 {
 protected:
 
 public:
+    /** @brief Variable for Pin1
+    */
     uint16_t PIN_1;           ///< Motor Pin1
+    /** @brief Variable for Pin2
+    */
     uint16_t PIN_2;           ///< Motor Pin2
+    /** @brief Motor init
+    */
     Motor (void);
+    /** @brief Method to set motor speed
+    *   @params PWM to set the motor speed
+    */
     void SetSpeed(uint16_t PWM);
+    /** @brief Method to get the current draw of motor driver
+    *   @return Current returns the motor current
+    */
     float getcurrent(void);
     
 
 };
-/** This class operates several print array funcitons with different inputs
- */
-
-
-
-
 
 #endif
